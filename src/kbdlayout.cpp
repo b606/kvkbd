a22964535bb5b9b26dcd3c9e615a76a052305206
b606 <7627533+b606@users.noreply.github.com>
2025-10-26 02:00:46 +0100

    Add class KbdLayout for marshalling QDBusArgument. The call to DBus interface org.kde.KeyboardLayouts/getLayoutsList returns an array of structure (sss). Ex. ([Argument: a(sss) {[Argument: (sss) "fr", "fr", "Français (variante)"]}]).

---
+++ src/kbdlayout.cpp
@@ -0,0 +1,108 @@
+// <one line to give the program's name and a brief idea of what it does.>
+// SPDX-FileCopyrightText: Copyright (C) 2025 b606 <b606@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "kbdlayout.h"
+#include <QDBusArgument>
+#include <QDBusMetaType>
+
+
+KbdLayout::KbdLayout(const QString& shortName, const QString& name,
+              const QString& variantName) : QObject()
+{
+    setShortName(shortName);
+    setName(name);
+    setVariantName(variantName);
+}
+
+KbdLayout::KbdLayout(const KbdLayout& kbdLayout) : QObject()
+{
+    setShortName(kbdLayout.shortName());
+    setName(kbdLayout.name());
+    setVariantName(kbdLayout.variantName());
+}
+
+KbdLayout::~KbdLayout()
+{}
+
+QString KbdLayout::name() const
+{
+    return m_name;
+}
+
+void KbdLayout::setName(const QString& name)
+{
+    if (m_name == name) {
+        return;
+    }
+
+    m_name = name;
+}
+
+QString KbdLayout::shortName() const
+{
+    return m_shortName;
+}
+
+void KbdLayout::setShortName(const QString& shortName)
+{
+    if (m_shortName == shortName) {
+        return;
+    }
+
+    m_shortName = shortName;
+}
+
+QString KbdLayout::variantName() const
+{
+    return m_variantName;
+}
+
+void KbdLayout::setVariantName(const QString& variantName)
+{
+    if (m_variantName == variantName) {
+        return;
+    }
+
+    m_variantName = variantName;
+}
+
+KbdLayout& KbdLayout::operator=(const KbdLayout& kbdLayout)
+{
+    if (&kbdLayout != this) {        
+        setShortName(kbdLayout.shortName());
+        setName(kbdLayout.name());
+        setVariantName(kbdLayout.variantName());
+    }
+    return *this;    
+}
+
+void KbdLayout::registerMetaType()
+{
+    qRegisterMetaType<KbdLayout>("KbdLayout");
+    qRegisterMetaType<QList<KbdLayout>>("QList<KbdLayout>");
+    qDBusRegisterMetaType<KbdLayout>();
+    qDBusRegisterMetaType<QList<KbdLayout>>();
+}
+
+
+QDBusArgument &operator<<(QDBusArgument &argument, const KbdLayout &kbdLayout)
+{
+  argument.beginStructure();    
+  argument << kbdLayout.m_shortName;    
+  argument << kbdLayout.m_name;    
+  argument << kbdLayout.m_variantName;    
+  argument.endStructure();     
+  return argument;    
+}
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, KbdLayout &kbdLayout)
+{
+  argument.beginStructure();    
+  argument >> kbdLayout.m_shortName;    
+  argument >> kbdLayout.m_name;    
+  argument >> kbdLayout.m_variantName;    
+  argument.endStructure();     
+  return argument;
+}
+
---
+++ src/kbdlayout.h
@@ -0,0 +1,110 @@
+// <one line to give the program's name and a brief idea of what it does.>
+// SPDX-FileCopyrightText: Copyright (C) 2025 b606 <b606@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef KBDLAYOUT_H
+#define KBDLAYOUT_H
+
+#include <QObject>
+#include <QDBusArgument>
+
+/**
+ * Class KbdLayout:
+ * Reply objects from the DBus interface org.kde.KeyboardLayouts.
+ * A call to the method "getLayoutsList" returns an array of three strings
+ * (signature: "a(sss)") which will be cast to a QList<KbdLayout>.
+ */
+class KbdLayout : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName)
+    Q_PROPERTY(QString shortName READ shortName WRITE setShortName)
+    Q_PROPERTY(QString variantName READ variantName WRITE setVariantName)
+
+public:
+    /**
+     * Default constructor
+     */
+    KbdLayout(const QString& shortName = "fr", const QString& name = "fr",
+              const QString& variantName = "Français");
+
+    /**
+     * Copy constructor
+     */
+    KbdLayout(const KbdLayout& kbdLayout);
+    
+    /**
+     * Destructor
+     */
+    ~KbdLayout();
+
+    /**
+     * @return the name
+     */
+    QString name() const;
+
+    /**
+     * @return the shortName
+     */
+    QString shortName() const;
+
+    /**
+     * @return the variantName
+     */
+    QString variantName() const;
+    
+    /**
+     * Copy operator= override.
+     * 
+     * @return the KbdLayout& .
+     */
+    KbdLayout& operator=(const KbdLayout& kbdLayout);
+
+    /**
+     * Marshalling operator<< to push a KbdLayout into a QDBusArgument.
+     * 
+     * @return the modified QDBusArgument& .
+     */
+    friend QDBusArgument &operator<<(QDBusArgument &argument, const KbdLayout &kbdLayout); 
+
+    /**
+     * Marshalling operator>> to load a KbdLayout from a QDBusArgument.
+     * 
+     * @return the const QDBusArgument&.
+     */
+    friend const QDBusArgument &operator>>(const QDBusArgument &argument, KbdLayout &kbdLayout);
+
+    static void registerMetaType();
+
+public Q_SLOTS:
+    /**
+     * Sets the name.
+     *
+     * @param name the new name
+     */
+    void setName(const QString& name);
+
+    /**
+     * Sets the shortName.
+     *
+     * @param shortName the new shortName
+     */
+    void setShortName(const QString& shortName);
+
+    /**
+     * Sets the variantName.
+     *
+     * @param variantName the new variantName
+     */
+    void setVariantName(const QString& variantName);
+
+private:
+    QString m_shortName;
+    QString m_name;
+    QString m_variantName;
+};
+
+Q_DECLARE_METATYPE(KbdLayout);
+Q_DECLARE_METATYPE(QList<KbdLayout>);
+
+#endif // KBDLAYOUT_H
